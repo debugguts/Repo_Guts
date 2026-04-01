4c887c1f88f38e944867540d55e652981554d829
Gustavo <gutss11ao@gmail.com>
2026-04-01 14:39:28 -0300

    Commit

--- index.py
+++ index.py
@@ -6,4 +6,5 @@ print (f"Bienvenidos, {hola} {Mundo}  probando")
 
 print ("Esto es un mensaje de prueba para github")
 
-print ("Esto es un saludo:"Hola Mundo"")
+print ("Esto es un saludo")
+print ("Commit")
